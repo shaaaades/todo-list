10e756f19c0987e803225475259cb84ac8b4d2de
Sheena <sheenaseciban.17@gmail.com>
2025-02-26 17:46:14 +0800

    Add task functionality

--- scripts/mainpage.js
+++ scripts/mainpage.js
@@ -1,15 +1,63 @@
-function addTask() {
-  let task = document.getElementById("add-task");
-  task.addEventListener("click", function() {
-    console.log("Clicked")
-    let addTaskModal = document.getElementById("add-task-modal");
-    if (addTaskModal.style.display === "none") {
-      addTaskModal.style.display = "block";
-    } 
-    else {
-      addTaskModal.style.display = "none";
-    }
-  })
-}
+let taskName, taskDate, taskPriority;
+const taskNameElem = document.querySelector("#task-name");
+const taskDateElem = document.querySelector("#task-date")
+const taskPriorityElem = document.querySelector("#task-priority");
+const addTask = document.getElementById("add-task-button");
 
-export default addTask();
+addTask.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  // Get the values of the input fields
+  taskName = taskNameElem.value;
+  taskDate = taskDateElem.value;
+  taskPriority = taskPriorityElem.value;
+
+  let taskDetails = [{
+    taskName,
+    taskDate,
+    taskPriority
+  }]
+
+  // Validate input fields when empty
+  if (taskName === "" || taskDate === "" || taskPriority === "") {
+    if (!document.getElementById("error-message")){
+      let errorMessage = document.createElement("p");
+      errorMessage.setAttribute("id", "error-message");
+      errorMessage.innerText = "Please fill in all fields to proceed."
+
+      addTask.parentNode.insertBefore(errorMessage, addTask.nextSibling);
+    } 
+    return;
+  } else {
+    // Remove the error message if it exists
+    let errorMessage = document.getElementById("error-message");
+    errorMessage.remove();
+  }
+
+  // Retrieve tasks before adding a new task
+  let retrievedTasks = localStorage.getItem("taskDetails");
+  let updatedTasks = JSON.parse(retrievedTasks);
+
+  if(retrievedTasks !== null) {
+    let tasks = [...updatedTasks, ...taskDetails];
+    localStorage.setItem("taskDetails", JSON.stringify(tasks));
+  } else {
+    localStorage.setItem("taskDetails", JSON.stringify(taskDetails));
+  }
+})
+
+// // To show add task modal when clicking add task icon
+// function addTask() {
+//   let task = document.getElementById("add-task");
+//   task.addEventListener("click", function() {
+//     let addTaskModal = document.getElementById("add-task-modal");
+//     if (addTaskModal.style.display === "none") {
+//       addTaskModal.style.display = "block";
+//     } 
+//     else {
+//       addTaskModal.style.display = "none";
+//     }
+//   })
+// }
+
+// export default addTask();
